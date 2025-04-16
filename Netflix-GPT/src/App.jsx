dbde201fa958e2c5b48fa4417b3569bbb04afbbf
Raunak Pal <raunakpal51441@gmail.com>
2025-04-16 13:31:31 +0530

    Learned Tailwind

---
+++ Netflix-GPT/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <p className="text-3xl font-bold underline text-center text-red-600">
+        This Side Raunak
+      </p>
+  )
+}
+
+export default App
